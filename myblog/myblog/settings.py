40f67747be0d8c9f551b550189e573596eed3117
kamdem Brice <kamdembrice770@gmail.com>
2025-04-20 11:44:53 +0100

    ajout de bluid.sh et retire staticfiles/ de .gitignore

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -85,11 +85,7 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default' : dj_database_url.parse(config('DATABASE_URL'))
+    'default' : dj_database_url.parse(config('DATABASE_URL')),
 }
 
 
